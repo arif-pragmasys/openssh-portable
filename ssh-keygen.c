5a4dbd136649dd945e6e455c24100cc33c5a4ab2
arif-pragmasys <Administrator@pragmasys.local>
2016-12-06 14:34:39 -0800

    error message has been modified based on whether nistP521 is supported
    issue # 413

--- ssh-keygen.c
+++ ssh-keygen.c
@@ -220,8 +220,13 @@ type_bits_valid(int type, const char *name, u_int32_t *bitsp)
 	else if (type != KEY_ECDSA && type != KEY_ED25519 && *bitsp < 1024)
 		fatal("Key must at least be 1024 bits");
 	else if (type == KEY_ECDSA && sshkey_ecdsa_bits_to_nid(*bitsp) == -1)
+#ifdef OPENSSL_HAS_NISTP521
 		fatal("Invalid ECDSA key length - valid lengths are "
 		    "256, 384 or 521 bits");
+#else
+		fatal("Invalid ECDSA key length - valid lengths are "
+			"256 or 384 bits");
+#endif
 #endif
 }
 
